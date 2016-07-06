5445f1242a373dfc2f9682814588a0f01d18a8bc
Joao Neto <jpsneto1@gmail.com>
2016-07-06 14:29:36 -0300

    update nested validator

--- app/validators/nested_attributes_uniqueness_validator.rb
+++ app/validators/nested_attributes_uniqueness_validator.rb
@@ -2,7 +2,7 @@ class NestedAttributesUniquenessValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value.map(&options[:field]).collect{|v| v.to_s.strip}.uniq.size == value.size
       # record.errors[attribute] << (options[:message] || 'must be unique')
-      duplicates = value - Hash[value.map{|obj| [obj[options[:field]].try(:strip), obj]}].values
+      duplicates = value - Hash[value.map{|obj| [obj[options[:field]].to_s.strip, obj]}].values
 
       # marca os itens duplicados no form
       duplicates.each { |obj| obj.errors[options[:field]] << I18n.t('errors.messages.taken') }
