c5874b6fc5786d2ecd1c0f2dcda19e717d9c0929
unknown <rogeriodti@hotmail.com>
2016-07-19 16:40:41 -0300

    init support json import surveys

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -45,7 +45,7 @@ class Survey < ActiveRecord::Base
     new_record.id = data_hash['id']
     new_record.name = data_hash['name']
     new_record.description = data_hash['description']
-    new_record.attempts_number = data_hash['attempts_number'] 
+    new_record.attempts_number = data_hash['attempts_number']
     new_record.active = data_hash['active']
 
     data_hash['questions'].each do |question|
--- app/views/admin/surveys/index.json.jbuilder
+++ app/views/admin/surveys/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@surveys) do |survey|
-  json.extract! survey, :id, :name, :description, :attempts_number, :finished, :active
+  json.extract! survey, :id, :name, :description, :attempts_number, :active
   json.url survey_url(survey, format: :json)
 end
